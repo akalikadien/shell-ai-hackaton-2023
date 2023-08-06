fbf3cdf4181b75b382f002258efa326e1a27243a
akalikadien <45177460+akalikadien@users.noreply.github.com>
2023-08-06 20:08:41 +0200

    updated readme, moved some stuff around in 2.additional_data, added approach for solving facility placement problem, added pyscipopt to conda environment

--- place_storage_and_refineries.py
+++ place_storage_and_refineries.py
@@ -12,198 +12,162 @@ storage depot j to refinery k.
 The depots have an annual capacity of 20000 tons, the refineries have an annual capacity of 100000 tons
 The maximum amount of depots is 25, the maximum amount of refineries is 5"""
 
-# WIP, finding algo to place depots and refineries
-# # 2. Use genetic algorithm
-# import os
-# import pandas as pd
-# import numpy as np
-# from deap import algorithms, base, creator, tools
-#
-# # Read the biomass data
-# biomass_data = pd.read_csv(os.path.join('dataset', '3.predictions', 'biomass_predictions.csv'))
-#
-# # Extract location indices and biomass availability for each year
-# locations = biomass_data['index'].values
-# biomass_2018 = dict(zip(locations, biomass_data['2018'].values))
-# biomass_2019 = dict(zip(locations, biomass_data['2019'].values))
-#
-# # Read the distance matrix data
-# distance_matrix_data = pd.read_csv(os.path.join('dataset', '1.initial_datasets', 'Distance_Matrix.csv'), index_col=0)
-# distance_matrix = np.array(distance_matrix_data)
-#
-# # Define the problem as a minimization problem
-# creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
-#
-# # Define the individual (solution) as a list of binary values (0 or 1) representing depot and biorefinery locations
-# creator.create("Individual", list, fitness=creator.FitnessMin)
-#
-# # Initialize the Genetic Algorithm toolbox
-# toolbox = base.Toolbox()
-#
-# # Attribute generator: randomly initialize an individual
-# toolbox.register("attr_bool", np.random.randint, 2)
-#
-# # Structure initializers: create individuals (solutions) and populations (collections of individuals)
-# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=len(locations) * 2)
-# toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-#
-# # Evaluation function: calculate the total cost and underutilization cost for an individual
-# def evaluate(individual):
-#     depot_indices = [idx for idx, gene in enumerate(individual) if gene == 1][:25]
-#     biorefinery_indices = [idx for idx, gene in enumerate(individual) if gene == 1][25:]
-#
-#     # Calculate transportation costs
-#     total_transport_cost = sum(distance_matrix[loc_idx-1][depot_idx-1] * biomass_2018[loc_idx+1] for loc_idx in range(len(locations)) for depot_idx in depot_indices) + \
-#                           sum(distance_matrix[depot_idx-1][bio_idx-1] * 20000 for depot_idx in depot_indices for bio_idx in biorefinery_indices)
-#
-#     # Calculate underutilization costs
-#     depot_underutilization_cost = sum((20000 - sum(biomass_2018[loc_idx+1] for loc_idx in range(len(locations)) if individual[loc_idx] == 1)) for depot_idx in depot_indices)
-#     biorefinery_underutilization_cost = sum((100000 - sum(20000 for depot_idx in depot_indices if individual[depot_idx] == 1)) for bio_idx in biorefinery_indices)
-#
-#     return total_transport_cost, depot_underutilization_cost + biorefinery_underutilization_cost
-#
-#
-#
-#
-# # Register the evaluation function to the toolbox
-# toolbox.register("evaluate", evaluate)
-#
-# # Genetic Operators
-# toolbox.register("mate", tools.cxTwoPoint)
-# toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-# toolbox.register("select", tools.selNSGA2)
-#
-# def main():
-#     # Population size
-#     pop_size = 100
-#
-#     # Number of generations
-#     ngen = 100
-#
-#     # Crossover probability
-#     cxpb = 0.5
-#
-#     # Mutation probability
-#     mutpb = 0.2
-#
-#     # Create the population and define the Hall of Fame (stores the best individuals in each generation)
-#     pop = toolbox.population(n=pop_size)
-#     hof = tools.ParetoFront()
-#
-#     # Run the Genetic Algorithm
-#     algorithms.eaSimple(pop, toolbox, cxpb, mutpb, ngen, halloffame=hof)
-#
-#     # Print the best individuals (solutions) found by the GA
-#     best_individuals = hof[0:5]  # Select the top 5 best individuals (non-dominated solutions)
-#     print("Optimal locations for depots and biorefineries:")
-#     for individual in best_individuals:
-#         depot_indices = [idx for idx, gene in enumerate(individual) if gene == 1][:25]
-#         biorefinery_indices = [idx for idx, gene in enumerate(individual) if gene == 1][25:]
-#         print("Depot locations:", depot_indices)
-#         print("Biorefinery locations:", biorefinery_indices)
-#         print("-------------------------")
-#
-# if __name__ == "__main__":
-#     main()
+# check https://scipbook.readthedocs.io/en/latest/flp.html for more info on the FLP
+# WIP, 1 initial implementation via pyscipopt
+import os
+import multiprocessing
+
+import pandas as pd
+from pyscipopt import Model, quicksum, multidict, SCIP_PARAMSETTING
 
 
-# # 1. use linear programming?
-# import os
-# import pandas as pd
-# from pulp import *
-#
-# def find_optimal_locations(biomass_file, distance_matrix_file, num_depots=25, num_biorefineries=5):
-#     # Read the biomass data
-#     biomass_data = pd.read_csv(biomass_file)
-#
-#     # Extract location indices and biomass availability for each year
-#     locations = biomass_data['index'].values
-#     biomass_2018 = dict(zip(locations, biomass_data['2018'].values))
-#     biomass_2019 = dict(zip(locations, biomass_data['2019'].values))
-#
-#     # Read the distance matrix data and set the first row and column as the index and columns
-#     distance_matrix_data = pd.read_csv(distance_matrix_file, index_col=0)
-#
-#     # Create a PuLP problem
-#     prob = LpProblem("Optimal_Location_Problem", LpMinimize)
-#
-#     # Variables: binary variables to indicate whether a depot/biorefinery is placed at a location
-#     depot_vars = LpVariable.dicts("Depot", locations, cat='Binary')
-#     biorefinery_vars = LpVariable.dicts("Biorefinery", locations, cat='Binary')
-#
-#     # Cost variables
-#     # Transportation costs
-#     depot_transportation_cost = LpVariable("Depot_Transport_Cost")
-#     biorefinery_transportation_cost = LpVariable("Biorefinery_Transport_Cost")
-#
-#     # Underutilization costs
-#     depot_underutilization_cost = LpVariable("Depot_Underutilization_Cost")
-#     biorefinery_underutilization_cost = LpVariable("Biorefinery_Underutilization_Cost")
-#
-#     # Objective function: minimize the total costs (transportation and underutilization)
-#     prob += depot_transportation_cost + biorefinery_transportation_cost + depot_underutilization_cost + biorefinery_underutilization_cost, "Total_Cost"
-#
-#     # Minimize transportation costs
-#     prob += depot_transportation_cost == lpSum([
-#         distance_matrix_data.at[loc, depot_loc] * biomass_2018[loc] * depot_vars[depot_loc]
-#         for loc in locations for depot_loc in locations
-#     ]) + lpSum([
-#         distance_matrix_data.at[depot_loc, loc] * 20000 * depot_vars[depot_loc] * biorefinery_vars[loc]
-#         for loc in locations for depot_loc in locations
-#     ]), "Depot_Transportation_Cost"
-#
-#     prob += biorefinery_transportation_cost == lpSum([
-#         distance_matrix_data.at[loc, biorefinery_loc] * 100000 * biorefinery_vars[loc]
-#         for loc in locations for biorefinery_loc in locations
-#     ]), "Biorefinery_Transportation_Cost"
-#
-#     # Minimize underutilization costs
-#     prob += depot_underutilization_cost == lpSum([
-#         (20000 - lpSum(biomass_2018[loc] * depot_vars[loc] + biomass_2019[loc] * depot_vars[loc] for loc in locations)) * depot_vars[loc]
-#         for loc in locations
-#     ]), "Depot_Underutilization_Cost"
-#
-#     prob += biorefinery_underutilization_cost == lpSum([
-#         (100000 - lpSum(20000 * depot_vars[loc] * biorefinery_vars[loc] for loc in locations)) * biorefinery_vars[loc]
-#         for loc in locations
-#     ]), "Biorefinery_Underutilization_Cost"
-#
-#     # Constraints
-#
-#     # Constraint 1: Total number of depots and biorefineries should not exceed the given limits
-#     prob += lpSum([depot_vars[loc] for loc in locations]) == num_depots, "Num_Depots"
-#     prob += lpSum([biorefinery_vars[loc] for loc in locations]) == num_biorefineries, "Num_Biorefineries"
-#
-#     # Constraint 2: Each location should have either a depot or a biorefinery
-#     for loc in locations:
-#         prob += depot_vars[loc] + biorefinery_vars[loc] == 1, f"Placement_Constraint_{loc}"
-#
-#     # Constraint 3: At least 80% of total biomass should be processed by biorefineries each year
-#     prob += lpSum([biomass_2018[loc] * biorefinery_vars[loc] + biomass_2019[loc] * biorefinery_vars[loc]
-#                    for loc in locations]) >= 0.8 * lpSum([biomass_2018[loc] + biomass_2019[loc]
-#                                                          for loc in locations]), "Min_Biorefinery_Biomass"
-#
-#     # Constraint 4: Amount of biomass leaving each harvesting site towards a depot should be less or equal to the biomass availability for that year
-#     for loc in locations:
-#         prob += lpSum([biomass_2018[loc] * depot_vars[loc] + biomass_2019[loc] * depot_vars[loc]
-#                        for loc in locations]) <= biomass_2018[loc] + biomass_2019[loc], f"Max_Harvest_Biomass_{loc}"
-#
-#     # Solve the problem
-#     prob.solve()
-#
-#     # Print the results
-#     print("Status:", LpStatus[prob.status])
-#     print("Optimal locations for depots:")
-#     for loc in locations:
-#         if depot_vars[loc].value() == 1:
-#             print(f"Depot at location {loc}")
-#     print("Optimal locations for biorefineries:")
-#     for loc in locations:
-#         if biorefinery_vars[loc].value() == 1:
-#             print(f"Biorefinery at location {loc}")
-#
-#
-# if __name__ == "__main__":
-#     biomass_file = os.path.join('dataset', '3.predictions', 'biomass_predictions.csv')
-#     distance_matrix_file = os.path.join('dataset', '1.initial_datasets', 'Distance_Matrix.csv')
-#     find_optimal_locations(biomass_file, distance_matrix_file)
+def get_optimal_thread_count(percentage=0.6):
+    available_cores = multiprocessing.cpu_count()
+    optimal_threads = int(available_cores * percentage)
+    return optimal_threads
+
+
+def calculate_transportation_cost(distance_matrix_data, depot_vars, biorefinery_vars, biomass_2018, biomass_2019, locations):
+    """Transportation cost is defined as the sum of the distance times amount of pellet or biomass transported"""
+    total_transport_cost = 0
+
+    for loc_idx in range(len(locations)):
+        loc = locations[loc_idx]
+        for depot_idx in range(len(locations)):
+            depot_loc = locations[depot_idx]
+            total_transport_cost += distance_matrix_data.iloc[loc_idx, depot_idx] * (biomass_2018[loc_idx] + biomass_2019[loc_idx]) * depot_vars[depot_loc]
+        for bio_idx in range(len(locations)):
+            bio_loc = locations[bio_idx]
+            total_transport_cost += distance_matrix_data.iloc[loc_idx, bio_idx] * (20000 * depot_vars[loc] + 100000 * biorefinery_vars[loc])
+
+    return total_transport_cost
+
+
+def calculate_underutilization_cost(depot_vars, biorefinery_vars, biomass_2018, biomass_2019, locations):
+    """Underutilization cost is defined as the capacity of the depot or biorefinery minus the amount of biomass or pellets transported to the depot or biorefinery"""
+    total_underutilization_cost = 0
+
+    for loc_idx in range(len(locations)):
+        loc = locations[loc_idx]
+        total_underutilization_cost += max(0, 20000 - (biomass_2018[loc_idx] + biomass_2019[loc_idx])) * depot_vars[loc]
+        total_underutilization_cost += max(0, 100000 - (biomass_2018[loc_idx] + biomass_2019[loc_idx])) * biorefinery_vars[loc]
+
+    return total_underutilization_cost
+
+
+def solve_optimization_problem(biomass_file, distance_matrix_file, year_1, year_2):
+    # ToDo: currently everything is named after 2018/2019 but should be year1/year2 since we want the algo to take any 2 years as input
+    # Read the biomass data
+    biomass_data = pd.read_csv(biomass_file)
+
+    # Extract location indices and biomass availability for each year
+    locations = biomass_data['index'].values
+    biomass_2018 = biomass_data[year_1].values
+    biomass_2019 = biomass_data[year_2].values
+
+    # Read the distance matrix data
+    distance_matrix_data = pd.read_csv(distance_matrix_file, index_col=0)
+
+    model = Model("Biomass_Optimization")
+
+    # Decision variables
+    depot_vars = {}
+    biorefinery_vars = {}
+
+    for loc in locations:
+        depot_vars[loc] = model.addVar(vtype="B", name=f"Depot_{loc}")
+        biorefinery_vars[loc] = model.addVar(vtype="B", name=f"Biorefinery_{loc}")
+
+    # Constraints
+    for loc in locations:
+        # adds constraints to ensure that at each location, either a depot or a biorefinery is placed, but not both.
+        model.addCons(depot_vars[loc] + biorefinery_vars[loc] <= 1, name=f"Placement_Constraint_{loc}")
+
+    # ensure that the total number of depots is 25 and the total number of biorefineries is 5
+    model.addCons(quicksum(depot_vars[loc] for loc in locations) == 25, name="Num_Depots")
+    model.addCons(quicksum(biorefinery_vars[loc] for loc in locations) == 5, name="Num_Biorefineries")
+
+    for loc in locations:
+        # This loop adds constraints to ensure that the sum of biomass transported to depots from all locations (2018 and 2019 combined) is less than or equal to the total biomass available at each location (2018 and 2019 combined).
+        model.addCons(quicksum(biomass_2018[loc] * depot_vars[loc] + biomass_2019[loc] * depot_vars[loc] for loc in locations) <= biomass_2018[loc] + biomass_2019[loc], name=f"Max_Harvest_Biomass_{loc}")
+
+    # This constraint ensures that the sum of biomass transported to biorefineries from all locations (2018 and 2019 combined) is at least 80% of the total biomass available at all locations (2018 and 2019 combined).
+    model.addCons(quicksum(biomass_2018[loc] * biorefinery_vars[loc] + biomass_2019[loc] * biorefinery_vars[loc] for loc in locations) >= 0.8 * quicksum(biomass_2018[loc] + biomass_2019[loc] for loc in locations), name="Min_Biorefinery_Biomass")
+
+    # Objective function: minimize total cost
+    # Calculate transportation cost and underutilization cost
+    total_transport_cost = calculate_transportation_cost(distance_matrix_data, depot_vars, biorefinery_vars,
+                                                         biomass_2018, biomass_2019, locations)
+    total_underutilization_cost = calculate_underutilization_cost(depot_vars, biorefinery_vars, biomass_2018,
+                                                                  biomass_2019, locations)
+    total_cost = total_transport_cost + total_underutilization_cost
+
+    model.setObjective(total_cost, "minimize")
+
+    # set to concurrent mode
+    model.setParam("parallel/mode", 1)
+    # Configure the number of solving threads
+    num_threads = get_optimal_thread_count()
+    model.setParam("parallel/maxnthreads", num_threads)
+
+    # Set the solver to optimization mode
+    model.setIntParam("presolving/maxrounds", 10)  # Adjust presolving aggressiveness
+    model.setHeuristics(SCIP_PARAMSETTING.AGGRESSIVE)  # Use aggresive
+
+    # Set solver's precision (number of decimal places)
+    # solver_precision = 4  # Adjust this value as needed
+    # model.setRealParam("numerics/feastol", 10 ** (-solver_precision))
+    # model.setRealParam("numerics/dualfeastol", 10 ** (-solver_precision))
+    # model.setRealParam("numerics/primalfeastol", 10 ** (-solver_precision))
+
+    # Solve the problem
+    model.optimize()
+
+    # Create matrices to store the flow values
+    flow_biomass_2018 = pd.DataFrame(index=locations, columns=locations)
+    flow_biomass_2019 = pd.DataFrame(index=locations, columns=locations)
+    flow_pellets_2018 = pd.DataFrame(index=locations, columns=locations)
+    flow_pellets_2019 = pd.DataFrame(index=locations, columns=locations)
+
+    # Store the flow values in the matrices
+    for loc_idx in range(len(locations)):
+        for depot_idx in range(len(locations)):
+            flow_biomass_2018.iloc[loc_idx, depot_idx] = biomass_2018[loc_idx] * model.getVal(
+                depot_vars[locations[depot_idx]])
+            flow_biomass_2019.iloc[loc_idx, depot_idx] = biomass_2019[loc_idx] * model.getVal(
+                depot_vars[locations[depot_idx]])
+            flow_pellets_2018.iloc[depot_idx, loc_idx] = 20000 * model.getVal(depot_vars[locations[loc_idx]])
+            flow_pellets_2019.iloc[depot_idx, loc_idx] = 20000 * model.getVal(depot_vars[locations[loc_idx]])
+
+    # Print the results
+    if model.getStatus() == "optimal":
+        print("Optimal solution found!")
+        print("Optimal locations for depots:")
+        for loc in locations:
+            if model.getVal(depot_vars[loc]) > 0.5:
+                print(f"Depot at location {loc}")
+        print("Optimal locations for biorefineries:")
+        for loc in locations:
+            if model.getVal(biorefinery_vars[loc]) > 0.5:
+                print(f"Biorefinery at location {loc}")
+        # save the optimal locations to a CSV file
+        optimal_locations = pd.DataFrame(index=locations, columns=['Depot', 'Biorefinery'])
+        for loc in locations:
+            optimal_locations.loc[loc, 'Depot'] = model.getVal(depot_vars[loc])
+            optimal_locations.loc[loc, 'Biorefinery'] = model.getVal(biorefinery_vars[loc])
+        optimal_locations.to_csv('optimal_locations.csv')
+    else:
+        print("No optimal solution found.")
+
+    # Save the flow matrices to CSV files
+    flow_biomass_2018.to_csv(f'flow_biomass_{year_1}.csv')
+    flow_biomass_2019.to_csv(f'flow_biomass_{year_2}.csv')
+    flow_pellets_2018.to_csv(f'flow_pellets_{year_1}.csv')
+    flow_pellets_2019.to_csv(f'flow_pellets_{year_2}.csv')
+
+
+if __name__ == "__main__":
+    biomass_file = os.path.join('dataset', '3.predictions', 'biomass_predictions.csv')
+    distance_matrix_file = os.path.join('dataset', '1.initial_datasets', 'Distance_Matrix.csv')
+    year1, year2 = '2018', '2019'
+    solve_optimization_problem(biomass_file, distance_matrix_file, year1, year2)
--- readme.md
+++ readme.md
@@ -18,10 +18,22 @@ conda activate shell-ai-hackaton
 ```
 
 ## Data
-
+1.initial_datasets/ contains the initial datasets that were supplied with the challenge
 * **Biomass_History.csv**:  A time-series of biomass availability in the state of Gujarat from year 2010 to 2017. We have considered arable land as a map of 2418 equisized grid blocks (harvesting sites). For ease of use, we have flattened the map and provided location index, latitude, longitude, and year wise biomass availability for each harvesting site
 * **Distance_Matrix.csv**:  The travel distance from source grid block to destination grid block, provided as a 2418 x 2418 matrix. Note that this is not a symmetric matrix due to U-turns, one-ways etc. that may result into different distances for ‘to’ and ‘fro’ journey between source and destination.
 * **sample_submission.csv**: Contains sample format for submission
+
+2.additional_data/ contains data that we use to enrich the initial datasets for prediction
+* max_temp: gridded data from IMD regarding the max temperatures in Gujarat
+* min_temp: gridded data from IMD regarding min temperatures in Gujarat
+* rainfall: gridded data from IMD regarding rainfall in Gujarat. Final_Rain_data contains interpolated 
+rainfall data for locations that did not have data. This is contained in the files Rain_Prediction{year}.csv
+* temperature predictions: contains interpolated data for locations that are missing in the gridded min_temp and max_temp from IMD.
+Currently the interpolation is based on the inverse weighted distance method.
+* pmgsy_facilities_gujarat.csv: gridded data on types of facilities in Gujarat
+
+3.predictions/ contains final files that will be merged into a submission file
+* biomass\_predictions.csv: predictions for biomass availability in 2018 and 2019
 
 ## Code
 
